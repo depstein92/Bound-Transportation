d943bffdafe6ed0d919680dcebd4a7832ab63ac9
Daniel Epstein <d.epstein92@gmail.com>
2020-09-15 21:20:33 -0400

    added main menu of user, needs styling

--- App.tsx
+++ App.tsx
@@ -1,15 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import appName from './app.json';
+import {
+  MenuComponent, 
+  userMenuOptions, 
+  driverMenuOptions,
+  adminMenuOptions
+} from './components/Menu';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <MenuComponent 
+          userMenuOptions={userMenuOptions}
+          driverMenuOptions={driverMenuOptions}
+          adminMenuOptions={adminMenuOptions}
+           />
+      </View>
+    </PaperProvider>
+    
   );
 }
+
+AppRegistry.registerComponent(JSON.stringify(appName), () => App);
 
 const styles = StyleSheet.create({
   container: {
