bd390f460925a9a5ee5fdc569be77944eca4584e
jeff-vincent <jeff.d.vincent@gmail.com>
2020-09-16 21:33:39 -0700

    Begin building out the backend.

--- backend/base_api.py
+++ backend/base_api.py
@@ -1,3 +1,5 @@
+import random
+import datetime
 from db import User
 from db import Driver
 from db import Trip
@@ -10,12 +12,43 @@ class BaseAPI:
 		self.driver = Driver
 		self.trip = Trip
 
+	def create_user(self, request):
+		user_name = request.form.get('user_name')
+		user_id = self.generate_user_id()
+		user = self.user(
+			user_name=user_name,
+			user_id=user_id)
+		user.save()
+		return 0
+
 	def get_user_by_name(self, name):
 		user = self.user.get(self.user.user_name==name)
+		return {'user_name':user.user_name, 'user_id':user.user_id}
 
-		return user
+	def create_trip(self, request):
+		start_location_lat_long = request.form.get('start_location_lat_long')
+		start_location_street_address = request.form.get('start_location_street_address')
+		destination_lat_long = request.form.get('destination_lat_long')
+		destination_street_address = request.form.get('destination_street_address')
+		current_lat_long = request.form.get('current_lat_long')
+		trip_has_begun = False
+		trip_has_completed = False
+		date_created = datetime.now()
+		trip = self.trip(
+			start_location_lat_long=start_location_lat_long,
+			start_location_street_address=start_location_street_address,
+			destination_lat_long=destination_lat_long,
+			destination_street_address=destination_street_address,
+			current_lat_long=current_lat_long,
+			trip_has_begun=trip_has_begun,
+			trip_has_completed=trip_has_completed)
+		trip.save(force_insert=True)
+		return 0
 
 	def get_trip_by_date_created(self, date):
 		trip = self.trip.get(self.trip.date_created==date)
-
 		return trip
+
+	def generate_user_id(self):
+		user_id = random.randint(1000000000, 9999999999)
+		return user_id
--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,57 @@
 import configparser
 from flask import Flask
 from flask import request
+from flask import jsonify
 from base_api import BaseAPI
-
 
 app = Flask('__main__')
 base = BaseAPI()
 config = configparser.ConfigParser()
 config.read('bound_transportation.cfg')
 
+
 @app.route('/', methods=['GET'])
-def index():
+def index(notification=None):
+	if notification:
+		return notification
+	else:
+		return 'service is up.'
 
-	return 'service is up.'
 
+@app.route('/create-user', methods=['POST'])
+def create_user():
+	user = base.create_user(request)
+	return jsonify(user)
+
+
+@app.route('/create-trip', methods=['POST'])
+def create_trip():
+	trip = base.create_trip(request)
+	return jsonify(trip)
+
+
+@app.route('/get-user-by-name/<user_name>', methods=['GET'])
+def get_user(user_name):
+		print(user_name)
+		if user_name:
+			try:
+				user = base.get_user_by_name(user_name)
+				return jsonify(user)
+			except Exception as e:
+				print(str(e))
+				return index(str(e))
+		else: 
+			return index(str(e))
+
+
+@app.route('/get-user-by-name', methods=['POST'])
+def get_user_by_post():
+	try:
+		user_name = request.form.get('user_name')
+		user = base.get_user_by_name(user_name)
+		return jsonify(user)
+	except Exception as e:
+		return index(str(e))
 
 
 if __name__ == '__main__':
